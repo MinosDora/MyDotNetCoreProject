b77e3b8357fe5bb85af3a90d9a19dad509d2885e
MinotaurosD <doraemon1970@163.com>
2020-11-17 13:34:30 +0800

    test of funciton pointer

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyDotNetCoreProject
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public unsafe static void Main(string[] args)
         {
+            delegate* managed<int, void> action = &Foo;
+            delegate* managed<int, void>[] actions = new delegate* managed<int, void>[1];
+            actions[0] = action;
+            for (int i = 0; i < actions.Length; i++)
+            {
+                actions[i](5);
+            }
+            IntPtr intPtr = (IntPtr)action;
+            action = (delegate* managed<int, void>)intPtr;
+            action(10);
             Console.WriteLine("Hello World!");
             Console.Read();
+        }
+
+        private static void Foo(int value)
+        {
+            Console.WriteLine(value); ;
         }
     }
 }
